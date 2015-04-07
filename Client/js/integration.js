941307279490b7e07a156a92c1133f55723a94a4
Aviad Hahami <aviadhahami@gmail.com>
2015-04-07 18:50:14 +0300

    integration module coming up

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -1,6 +1,24 @@
-$(document).ready(function(){
-    var socket = io();
-    $('myButt').click(function(e){
-        alert(typeof e);
+$(document).ready(function() {
+    var url = "https://hackidc2015.imrapid.com/message";
+    
+    //message output
+    $('#myButt').click(function(e) {
+        var message = $('#chatInput').val();
+        var clientData = {
+            room: '2',
+            UID: '2',
+            message: message
+        };
+        $.post(url, clientData, function(data) {
+            console.log(data);
+        });
+    });
+
+    //message feed
+    io.on('newMessage', function(data) {
+        $('<div />',{
+            class : 'col-xs-12 user_msg',
+            text : data.message,
+        }).appendTo('#chatBody');
     });
 });
