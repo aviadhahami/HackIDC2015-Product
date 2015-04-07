5a1e603204f690d7c50c76fbf12cf7bbe95e90ba
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 02:02:06 +0300

    lol forgot return statement :lollipop:

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     //message output
     $('#sendMsg').click(function(){
-        var chatMsg = $('#chatMsg').val();
+        var chatMsg = $('#textBox').val();
         var msg = {
             message : chatMsg,
             room : roomID
@@ -26,16 +26,17 @@ $(document).ready(function() {
     var io = createIO(projectName, roomID);
 
     function generateCurrentBlob(data){
-     var d = new Date();
-     var dateString = (d.getHours()+1) + ':' + (d.getMinutes());
-     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name +',' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.msg+'</p></div></div></div>';
-    };
+       var d = new Date();
+       var dateString = (d.getHours()+1) + ':' + (d.getMinutes());
+       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name +',' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.msg+'</p></div></div></div>';
+       return _htmlTemplateString;
+   };
 
 
-     io.on('newMsg', function(data) {
-        var outputHTMLString = generateCurrentBlob(data);
-        alert(data);
-        console.log(data,outputHTMLString);
+   io.on('newMsg', function(data) {
+    var outputHTMLString = generateCurrentBlob(data);
+    alert(data);
+    console.log(data,outputHTMLString);
         //$('.chat_body').append(outputHTMLString);
     });
 });
