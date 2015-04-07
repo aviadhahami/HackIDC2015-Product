1e16fba64eb5a025ef4dc8f379b57440201e7729
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 02:13:36 +0300

    Done chat integration on Client side

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -9,12 +9,13 @@ $(document).ready(function() {
 
     //message output
     $('#sendMsg').click(function(){
-        var chatMsg = $('#textBox').val();
+        var chatMsg = $('#chatMsg').val();
+        $('#chatMsg').val('');
         var msg = {
             message : chatMsg,
             room : roomID
         };
-        console.log('sending message object.. msg is' + chatMsg);
+        console.log('sending message object.. msg is ' + chatMsg);
 
         $.post(url,msg,function(data,status){
             console.log('data: ' + data + 'status : ' + status + 'from the POST');
@@ -27,16 +28,18 @@ $(document).ready(function() {
 
     function generateCurrentBlob(data){
        var d = new Date();
-       var dateString = (d.getHours()+1) + ':' + (d.getMinutes());
-       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name +',' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.msg+'</p></div></div></div>';
+       var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+       var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+       var dateString = hours + ':' + minutes;
+       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">David, ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
        return _htmlTemplateString;
    };
 
 
    io.on('newMsg', function(data) {
     var outputHTMLString = generateCurrentBlob(data);
-    alert(data);
-    console.log(data,outputHTMLString);
-        //$('.chat_body').append(outputHTMLString);
-    });
+   // alert(data);
+    console.log(data);//,outputHTMLString);
+   $('.chat_body').append(outputHTMLString);
+});
 });
