9c0b0978dbc6e2f0dc2d53339f5fef55bb84a813
Aviad Hahami <aviadhahami@gmail.com>
2015-04-07 19:48:36 +0300

    removed socket.io

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-    var url = "https://hackidc2015.imrapid.com/message";
-    
+
+    //global data
+    var url = "https://hackidc2015.imrapid.io/message";
+    var roomID = "2";
+    var projectName = "hackidc2015";
+
+
+
     //message output
     $('#myButt').click(function(e) {
         var message = $('#chatInput').val();
         var clientData = {
-            room: '2',
+            room: roomID,
             UID: '2',
             message: message
         };
@@ -14,11 +20,10 @@ $(document).ready(function() {
         });
     });
 
+
     //message feed
+    var io = createIO(projectName, roomID);
     io.on('newMessage', function(data) {
-        $('<div />',{
-            class : 'col-xs-12 user_msg',
-            text : data.message,
-        }).appendTo('#chatBody');
+        console.log(data.message);
     });
 });
