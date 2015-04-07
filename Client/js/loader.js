3f9a7e293b43a475dc5c9d9438d961a77124dafe
Ariel Verber <opiedesigns@gmail.com>
2015-04-07 23:43:29 +0300

    added onboarding

---
+++ Client/js/loader.js
@@ -0,0 +1,11 @@
+
+//$(document).ready(function () {
+$(".username_form").submit(function ( event ){
+    event.preventDefault();
+    $(".username").fadeOut();
+    $(".loader").fadeIn();
+    setTimeout( function(){
+        $('.shader').fadeOut();
+    },4000);
+
+});
