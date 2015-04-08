70688e2f89342f96aa4db67cc83682e2df57d855
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 17:28:47 +0300

    Started integration module

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -1,7 +1,13 @@
 
 $(document).ready(function() {
 
+    //Managment server integration module//
 
+
+
+    //end of server integration module//
+
+    
     //global data
     var url = 'https://hackidc2015.imrapid.io/message';
     var roomID = 'david';
@@ -22,19 +28,19 @@ $(document).ready(function() {
 
     //message output
     $('#sendMsg').click(function(){
-        chatMsg = $('#chatMsg').val();
-        $('#chatMsg').val('');
-        var msg = {
-            name: window.clientName,
-            message : chatMsg,
-            room : roomID,
-            userID : userID
-        };
-        console.log('sending message object.. msg is ',msg);
+      chatMsg = $('#chatMsg').val();
+      $('#chatMsg').val('');
+      var msg = {
+        name: window.clientName,
+        message : chatMsg,
+        room : roomID,
+        userID : userID
+      };
+      console.log('sending message object.. msg is ',msg);
 
-        $.post(url,msg,function(data,status){
-            console.log('data: ' + data + 'status : ' + status + 'from the POST');
-        });
+      $.post(url,msg,function(data,status){
+        console.log('data: ' + data + 'status : ' + status + 'from the POST');
+      });
     });
 
 
@@ -42,12 +48,12 @@ $(document).ready(function() {
     var io = createIO(projectName, roomID);
 
     function generateCurrentBlob(data){
-       var d = new Date();
-       var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-       var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-       var dateString = hours + ':' + minutes;
-       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="avatars/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
-       return _htmlTemplateString;
+     var d = new Date();
+     var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+     var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+     var dateString = hours + ':' + minutes;
+     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="avatars/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
+     return _htmlTemplateString;
    };
 
 
@@ -56,5 +62,5 @@ $(document).ready(function() {
    // alert(data);
     console.log(data);//,outputHTMLString);
    $('.chat_body').append(outputHTMLString);
-});
-});
+ });
+ });
