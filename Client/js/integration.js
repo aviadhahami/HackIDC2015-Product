88c56d7068055daed5d19cb4a761ccc04d98ae43
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 16:42:07 +0300

    added userID

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     console.log(window.clientName);
     console.log('got JQ on via integration');
 
-
+    var userID = '2';
 
     //message output
     $('#sendMsg').click(function(){
@@ -25,7 +25,8 @@ $(document).ready(function() {
         var msg = {
             name: window.clientName,
             message : chatMsg,
-            room : roomID
+            room : roomID,
+            userID : userID
         };
         console.log('sending message object.. msg is ',msg);
 
@@ -39,19 +40,19 @@ $(document).ready(function() {
     var io = createIO(projectName, roomID);
 
     function generateCurrentBlob(data){
-     var d = new Date();
-     var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-     var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-     var dateString = hours + ':' + minutes;
-     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="avatars/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
-     return _htmlTemplateString;
- };
+       var d = new Date();
+       var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+       var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+       var dateString = hours + ':' + minutes;
+       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="avatars/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
+       return _htmlTemplateString;
+   };
 
 
- io.on('newMsg', function(data) {
+   io.on('newMsg', function(data) {
     var outputHTMLString = generateCurrentBlob(data);
    // alert(data);
     console.log(data);//,outputHTMLString);
- $('.chat_body').append(outputHTMLString);
+   $('.chat_body').append(outputHTMLString);
 });
 });
