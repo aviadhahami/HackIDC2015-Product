48e2fa14d722697e75edf61d187d4c38e36b0f42
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 03:55:51 +0300

    Added random name generator
    if a user fails to supply a name -> we supply it with one ! :dancer:

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -6,17 +6,14 @@ $(document).ready(function() {
     var roomID = 'david';
     var projectName = 'hackidc2015';
     var chatMsg ='';
-    var clientName ='';
 
 
     //////////////
     // Pre Chat //
     //////////////
 
-    $('.username_form').submit(function(){
-     clientName = $('#namePick').val();
- });
-    console.log(clientName);
+
+    console.log(window.clientName);
     console.log('got JQ on via integration');
 
 
@@ -26,7 +23,7 @@ $(document).ready(function() {
         chatMsg = $('#chatMsg').val();
         $('#chatMsg').val('');
         var msg = {
-            name: clientName,
+            name: window.clientName,
             message : chatMsg,
             room : roomID
         };
@@ -42,19 +39,19 @@ $(document).ready(function() {
     var io = createIO(projectName, roomID);
 
     function generateCurrentBlob(data){
-       var d = new Date();
-       var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-       var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-       var dateString = hours + ':' + minutes;
-       var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
-       return _htmlTemplateString;
-   };
+     var d = new Date();
+     var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+     var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+     var dateString = hours + ':' + minutes;
+     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
+     return _htmlTemplateString;
+ };
 
 
-   io.on('newMsg', function(data) {
+ io.on('newMsg', function(data) {
     var outputHTMLString = generateCurrentBlob(data);
    // alert(data);
     console.log(data);//,outputHTMLString);
-   $('.chat_body').append(outputHTMLString);
+ $('.chat_body').append(outputHTMLString);
 });
 });
--- Client/js/uiScripts.js
+++ Client/js/uiScripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-	
+
     //Overriding "ENTER" key for send msg
     $('#chatMsg').on('keyup',function(e){
-        if (e.which == 13){
-            $('#sendMsg').click();
-            e.preventDefault();
-        }
+    	if (e.which == 13){
+    		$('#sendMsg').click();
+    		e.preventDefault();
+    	}
+    });
+    $('.username_form').submit(function(){
+    	window.clientName = $('#namePick').val().length < 4 ? chance.name() : $('#namePick').val();
     });
 });
