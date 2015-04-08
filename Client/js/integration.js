82724d4e871f5090abddefcf1c5febd41912c692
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 18:25:53 +0300

    added connection function

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     var localID = '';
     var connectionStatus = '';
 
-
+    window.initiatePrimaryConnection = function(){
     var getReqDataString = 'rid=' + connectionFlag + '&bid=' + bid; //connection GET request string
     $.getJSON(managmentServerUrl,getReqDataString).done(function(res){
       consosle.log(res);
@@ -35,7 +35,7 @@ $(document).ready(function() {
       console.log(e);
     });
 
-
+  };
     //end of server integration module//
 
 
@@ -61,26 +61,26 @@ $(document).ready(function() {
     //message output
     $('#sendMsg').click(function(){
 
-        chatMsg = $('#chatMsg').val();
-        $('#chatMsg').val('');
-        var msg = {
-            name: window.clientName,
-            message : chatMsg,
-            room : roomID,
-            userID : userID,
-	userImg : userImg
-        };
-        console.log('sending message object.. msg is ',msg);
-        var outputHTMLString = generateCurrentBlob(msg);
+      chatMsg = $('#chatMsg').val();
+      $('#chatMsg').val('');
+      var msg = {
+        name: window.clientName,
+        message : chatMsg,
+        room : roomID,
+        userID : userID,
+        userImg : userImg
+      };
+      console.log('sending message object.. msg is ',msg);
+      var outputHTMLString = generateCurrentBlob(msg);
 
-        $('.chat_body').append(outputHTMLString);
-        ScrollFix();
-        $.post(url,msg,function(data,status){
-            console.log('data: ' + data + 'status : ' + status + 'from the POST');
-            if (status === "success") {
-                $('.timestamp').append('V');
-            }
-        });
+      $('.chat_body').append(outputHTMLString);
+      ScrollFix();
+      $.post(url,msg,function(data,status){
+        console.log('data: ' + data + 'status : ' + status + 'from the POST');
+        if (status === "success") {
+          $('.timestamp').append('V');
+        }
+      });
     });
 
 
@@ -94,27 +94,27 @@ $(document).ready(function() {
      var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
      var dateString = hours + ':' + minutes;
 
-   
-       var d = new Date();
-       var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-       var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-       var dateString = hours + ':' + minutes;
-        var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src=' + data.userImg +' alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
+     
+     var d = new Date();
+     var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+     var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+     var dateString = hours + ':' + minutes;
+     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src=' + data.userImg +' alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
      return _htmlTemplateString;
 
    };
 
-    function ScrollFix() {
-        $(".chat_body").scrollTop($(".chat_body")[0].scrollHeight);
-    }
+   function ScrollFix() {
+    $(".chat_body").scrollTop($(".chat_body")[0].scrollHeight);
+  }
 
-   io.on('newMsg', function(data) {
+  io.on('newMsg', function(data) {
     var outputHTMLString = generateCurrentBlob(data);
    // alert(data);
 
- $('.chat_body').append(outputHTMLString);
-     ScrollFix();
+   $('.chat_body').append(outputHTMLString);
+   ScrollFix();
 
  });
- });
+});
 
