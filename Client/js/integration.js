69e3bfb20e9511b62f96f4dd2258b1958e99a51b
Aviad Hahami <aviadhahami@gmail.com>
2015-04-08 03:08:39 +0300

    Added welcome message stub & name pull

--- Client/js/integration.js
+++ Client/js/integration.js
@@ -8,23 +8,36 @@ $(document).ready(function() {
         }
     });
 
-    console.log('got JQ on via integration');
-
     //global data
     var url = 'https://hackidc2015.imrapid.io/message';
     var roomID = 'david';
     var projectName = 'hackidc2015';
+    var chatMsg ='';
+    var clientName ='';
+
+
+    //////////////
+    // Pre Chat //
+    //////////////
+
+    $('.username_form').submit(function(){
+       clientName = $('#namePick').val();
+   });
+    console.log(clientName);
+    console.log('got JQ on via integration');
+
 
 
     //message output
     $('#sendMsg').click(function(){
-        var chatMsg = $('#chatMsg').val();
+        chatMsg = $('#chatMsg').val();
         $('#chatMsg').val('');
         var msg = {
+            name: clientName,
             message : chatMsg,
             room : roomID
         };
-        console.log('sending message object.. msg is ' + chatMsg);
+        console.log('sending message object.. msg is ',msg);
 
         $.post(url,msg,function(data,status){
             console.log('data: ' + data + 'status : ' + status + 'from the POST');
@@ -40,7 +53,7 @@ $(document).ready(function() {
      var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
      var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
      var dateString = hours + ':' + minutes;
-     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">David, ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
+     var _htmlTemplateString = '<div class="col-xs-12 user_msg"><div class="media message-box"><div class="media-left"><img class="media-object user-profile-in-chat" src="img/green.png" alt="general_id" style="width: 35px; height: 35px;"></div><div class="media-body"><h4 class="media-heading timestamp" id="top-aligned-media">'+ data.name+', ' + dateString+'<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4><p>'+ data.message+'</p></div></div></div>';
      return _htmlTemplateString;
  };
 
