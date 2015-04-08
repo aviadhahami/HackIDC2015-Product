bee284cfb7a3d132ea129d57eaeedb2e7e0dd339
Ariel Verber <opiedesigns@gmail.com>
2015-04-08 10:43:17 +0300

    Moved avatars to "avatars" folder, fixed responsive drawer

--- Client/js/uiScripts.js
+++ Client/js/uiScripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     //Overriding "ENTER" key for send msg
-    $('#chatMsg').on('keyup',function(e){
+    $('.chat_input_box').on('keyup',function(e){
     	if (e.which == 13){
     		$('#sendMsg').click();
     		e.preventDefault();
@@ -9,5 +9,21 @@ $(document).ready(function() {
     });
     $('.username_form').submit(function(){
     	window.clientName = $('#namePick').val().length < 4 ? chance.name() : $('#namePick').val();
+    });
+
+    var toggle = 0;
+    $('#open-button').click(function(){
+        if(toggle === 0){
+            $(".gif_drawer").animate({
+            top: "-=1600px",
+            }, 1000 );
+            toggle = 1;
+        }
+        else{
+            $(".gif_drawer").animate({
+            top: "+=1600px",
+            }, 1000 );
+            toggle = 0;
+        }
     });
 });
